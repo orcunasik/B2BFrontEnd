a05b38e97dbb93d7882ed7bb4d8cef63d89ff469
Orçun Aşık <asikorcun@gmail.com>
2023-06-06 23:49:13 +0300

    Module and component structures were created for admin and UI in the project. Admin theme added.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutsComponent } from './admin/layouts/layouts.component';
+import { HomeComponent } from './admin/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:LayoutsComponent,
+    children:[
+      {
+        path:'',
+        component:HomeComponent,
+        loadChildren: ()=> import('./admin/home/home.module')
+        .then(m => m.HomeModule)
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
