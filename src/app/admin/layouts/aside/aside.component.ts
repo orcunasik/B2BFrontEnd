9d0c70af9bb6b2c4ed2114c90ce53a49d7ab497b
Orçun Aşık <asikorcun@gmail.com>
2023-06-17 17:18:41 +0300

    Added line of code that retrieves userName from adminDecode service.

--- src/app/admin/layouts/aside/aside.component.ts
+++ src/app/admin/layouts/aside/aside.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AdminDecodeService } from '../../login/services/admin-decode.service';
 
 @Component({
   selector: 'app-aside',
   templateUrl: './aside.component.html',
   styleUrls: ['./aside.component.scss']
 })
-export class AsideComponent {
+export class AsideComponent implements OnInit {
+  userName: string = "";
 
+  constructor(
+    private adminDecodeService: AdminDecodeService
+  
+  ){ }
+  ngOnInit(): void {
+    this.userName = this.adminDecodeService.getUserName();
+  }
 }
