baf27e1c5d174e277775997a185bd905f3d39da9
Orçun Aşık <asikorcun@gmail.com>
2023-06-18 01:22:07 +0300

    Created delete method

--- src/app/admin/products/services/product.service.ts
+++ src/app/admin/products/services/product.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
+import { ProductModel } from '../models/product-model';
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +15,9 @@ export class ProductService {
   getList(){
     let api = this.apiUrl + "Products/GetList";
     return this.httpClient.get(api);
+  }
+  delete(product:ProductModel){
+    let api = this.apiUrl + "Products/Delete";
+    return this.httpClient.post(api,product);
   }
 }
