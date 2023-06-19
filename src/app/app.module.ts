d291f07ace36c554923b79fed1164c496ff90266
Orçun Aşık <asikorcun@gmail.com>
2023-06-19 19:12:51 +0300

    Added provide for the created authinceptor.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule} from '@angular/common/http'
+import { HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminModule } from './admin/admin.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
+import { AuthInterceptor } from './admin/login/interceptor/auth.interceptor';
 
 
 @NgModule({
@@ -26,6 +27,9 @@ import { ToastrModule } from 'ngx-toastr';
   providers: [
     {
       provide: 'apiUrl',useValue:'https://localhost:7146/api/'
+    },
+    {
+      provide: HTTP_INTERCEPTORS,useClass:AuthInterceptor,multi:true
     }
   ],
   bootstrap: [AppComponent]
