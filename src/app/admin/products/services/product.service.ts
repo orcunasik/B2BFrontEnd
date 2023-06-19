8e80c8afc5ed8cc2424324fdfdf0a8346fec81b9
Orçun Aşık <asikorcun@gmail.com>
2023-06-19 19:09:04 +0300

    Created update and getbyid methods for  product update.

--- src/app/admin/products/services/product.service.ts
+++ src/app/admin/products/services/product.service.ts
@@ -16,12 +16,24 @@ export class ProductService {
     let api = this.apiUrl + "Products/GetList";
     return this.httpClient.get(api);
   }
+
+  getById(productId:number){
+    let api = this.apiUrl + "Products/GetById/" + productId;
+    return this.httpClient.get(api);
+  }
+
   delete(product:ProductModel){
     let api = this.apiUrl + "Products/Delete";
     return this.httpClient.post(api,product);
   }
+
   add(product:ProductModel){
     let api = this.apiUrl + "Products/Add";
     return this.httpClient.post(api,product);
+  }
+
+  update(product:ProductModel){
+    let api = this.apiUrl + "Products/Update";
+    return this.httpClient.put(api,product);
   }
 }
