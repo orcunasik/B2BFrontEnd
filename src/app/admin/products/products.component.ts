2091a9731a3112dd92205b3c33e9c8798204f19a
Orçun Aşık <asikorcun@gmail.com>
2023-06-20 17:31:55 +0300

    CRUD methods have been refoctored.

--- src/app/admin/products/products.component.ts
+++ src/app/admin/products/products.component.ts
@@ -5,6 +5,7 @@ import { ProductModel } from './models/product-model';
 import { ToastrService } from 'ngx-toastr';
 import { NgForm } from '@angular/forms';
 import { HelperService } from 'src/app/services/helper.service';
+import { catchError, tap, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-products',
@@ -32,49 +33,81 @@ export class ProductsComponent implements OnInit {
     let title = "Ürünler";
     this.helperService.exportExcel(element,title);
   }
+
   getList(){
-    this.productService.getList().subscribe((res : any)=>{
-      this.products = res.data;
-    },(err) =>{
-      this.errorService.errorHandler(err);
-    })
+    this.productService.getList()
+      .pipe(
+        catchError(err =>{
+          this.errorService.errorHandler(err);
+          return throwError(() => err);
+        })
+      )
+      .subscribe((res : any)=>{
+        this.products = res.data;
+      });
   }
+
   delete(product : ProductModel){
-    this.productService.delete(product).subscribe((res:any)=>{
-      this.toastrService.info(res.message);
-      this.getList();
-    },(err)=>{
-      this.errorService.errorHandler(err);
-    });
+    this.productService.delete(product)
+      .pipe(
+        tap((res : any)=>{
+          this.toastrService.info(res.message);
+          this.getList();
+        }),
+        catchError(err =>{
+          this.errorService.errorHandler(err);
+          return throwError(() => err);
+        })
+      )
+    .subscribe();
   }
+
   add(addForm : NgForm){
     let product : ProductModel = new ProductModel();
     product.name = addForm.value.productName;
     product.id = 0;
-    this.productService.add(product).subscribe((res:any)=>{
-      this.toastrService.success(res.message);
-      this.getList();
-      addForm.reset();
-    },(err)=>{
-      this.errorService.errorHandler(err);
-    });
+    this.productService.add(product)
+      .pipe(
+        tap((res :any) =>{
+          this.toastrService.success(res.message);
+          this.getList();
+          addForm.reset();
+          document.getElementById("addModelCloseBtn").click();
+        }),
+        catchError(err => {
+          this.errorService.errorHandler(err);
+          return throwError(() => err);
+        })
+      )
+      .subscribe();
   }
 
   getProduct(product:ProductModel){
-    this.productService.getById(product.id).subscribe((res:any)=>{
-      this.product = res.data;
-    },(err)=>{
-      this.errorService.errorHandler(err);
-    });
+    this.productService.getById(product.id)
+      .pipe(
+        catchError(err =>{
+          this.errorService.errorHandler(err);
+          return throwError(() =>err);
+        })
+      )
+      .subscribe((res:any)=>{
+        this.product = res.data;
+      });
   }
 
   update(){
-    this.productService.update(this.product).subscribe((res:any)=>{
-      this.toastrService.success(res.message);
-      this.getList();
-      document.getElementById("updateModelCloseBtn").click();
-    },(err)=>{
-      this.errorService.errorHandler(err);
-    });
+    this.productService.update(this.product)
+      .pipe(
+        tap((res: any)=>{
+          this.toastrService.success(res.message);
+          this.getList();
+          document.getElementById("updateModelCloseBtn").click();
+        }),
+        catchError(err => {
+          this.errorService.errorHandler(err);
+          return throwError(() => err);
+        })
+      )
+    .subscribe();
   }
 }
